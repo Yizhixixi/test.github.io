10aa207712540b4f9b3bfe4d55655b2627ebd663
沉默王二 <www.qing_gee@163.com>
2020-02-06 06:38:03 +0800

    更新css和js

--- assets/js/lock.js
+++ assets/js/lock.js
@@ -16,6 +16,52 @@ var os = function () {
   }
 }()
 
+// Window Scroll
+var windowScroll = function () {
+    $(window).scroll(function () {
+
+        var scrollPos = $(this).scrollTop();
+        
+        var system ={win : false,mac : false,xll : false};
+        //检测平台
+        var p = navigator.platform;
+        system.win = p.indexOf("Win") == 0;
+        system.mac = p.indexOf("Mac") == 0;
+        system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+        //判断平台类型
+        if(system.win||system.mac||system.xll){
+            if ($(window).scrollTop() > 70)
+            {
+                $('.site-header').addClass('site-header-nav-scrolled');
+                $('.icon-logo').addClass('site-header-nav-scrolled');
+
+            } else {
+                $('.site-header').removeClass('site-header-nav-scrolled');
+                $('.icon-logo').removeClass('site-header-nav-scrolled');
+
+            }
+        }else{
+            //如果是手机则将顶栏移除界面
+            if ($(window).scrollTop() > 40) 
+            {
+                $('.site-header').addClass('site-header-nav-scrolled-ph');
+            } else {
+                $('.site-header').removeClass('site-header-nav-scrolled-ph');
+            }
+        }
+ });
+};
+
+function headerSubmenu() {
+    $('.site-header-nav').find('.site-header-nav-item').each(function() {
+        $(this).hover(function() {
+            $(this).find('.submenu').show()
+        }, function() {
+            $(this).find('.submenu').hide()
+        })
+    })
+};
+
 function getCookie(name) {
 	var value = "; " + document.cookie;
 	var parts = value.split("; " + name + "=");
@@ -38,6 +84,8 @@ var articleSelector = 'article.post.container.need';
 
 // DOM 完全就绪时执行
 $(function() {
+	windowScroll();
+    headerSubmenu();
 	// 找到文章所在的容器
 	var $article = $(articleSelector);
 	if ($article.length > 0) {
