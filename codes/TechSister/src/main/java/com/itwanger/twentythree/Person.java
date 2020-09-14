15491c5bc40cc8ad568a1d0070e27ab466972fc3
沉默王二 <www.qing_gee@163.com>
2020-09-14 12:40:08 +0800

    数据结构和算法

--- codes/TechSister/src/main/java/com/itwanger/twentythree/Person.java
+++ codes/TechSister/src/main/java/com/itwanger/twentythree/Person.java
@@ -8,6 +8,15 @@ public class Person {
     private int age;
     private int sex;
 
+    public Person() {
+    }
+
+    public Person(String name, int age, int sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
     private void eat() {
 
     }
@@ -20,8 +29,19 @@ public class Person {
 
     }
 
+    public void initialize(String n, int a, int s) {
+        name = n;
+        age = a;
+        sex = s;
+    }
+
     public static void main(String[] args) {
-        Person person = new Person();
+        Person person = new Person("沉默王二",18,1);
+
+        person.initialize("沉默王二",18,1);
+
+        new Person().initialize("沉默王二",18,1);
+
         person.name = "沉默王二";
         person.age = 18;
         person.sex = 1;
@@ -29,6 +49,8 @@ public class Person {
         System.out.println(person.name);
         System.out.println(person.age);
         System.out.println(person.sex);
+
+        Person person1 = new Person(), person2 = new Person();
     }
 }
 
